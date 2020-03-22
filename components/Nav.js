ba70671edcd48b96424285126fe5e318eec54af7
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-03-21 20:00:11 -0400

    init

---
+++ components/Nav.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
+
+const Nav = () => (
+  <NavStyles>
+    <Link href="/items">
+      <a>Items</a>
+    </Link>
+    <br />
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>Sign Up</a>
+    </Link>
+    <br />
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
+);
+export default Nav;
